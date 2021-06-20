a10903dbc7702a23e0d24523f4620af7039de7ad
hamidb80 <hr.bolouri@gmail.com>
2021-06-21 00:39:32 +0430

    complete d7

--- advent-of-code/2020/d7/mainr.nim
+++ advent-of-code/2020/d7/mainr.nim
@@ -1,5 +1,4 @@
-import sequtils, strutils, tables, sets, hashes
-# import print
+import strutils, tables, sets, hashes
 
 # functionalities ------------------------------------------------
 
@@ -7,8 +6,8 @@ type Bag = object
   count: int
   color: string
 
-func hash(b: Bag): Hash =
-  hash b.color
+func hash(b: Bag): Hash = hash b.color
+func `==`(a,b: Bag): bool = a.color == b.color
 
 func parseBags(bags: seq[string]): HashSet[Bag] =
   for bag in bags:
@@ -28,15 +27,40 @@ func parseRule(line: string): tuple[mainBagColor: string, bags: HashSet[Bag]] =
 # preparing data -------------------------------------------------
 
 var rules: Table[string, HashSet[Bag]]
+const targetBag = Bag(color:"shiny gold", count: 1)
 for line in "./input.txt".lines:
   let rule = line.parseRule
   rules[rule.mainBagColor] = rule.bags
 
 # code -----------------------------------------------------------
 
+func isParentOf(
+  table:var Table[string, HashSet[Bag]], 
+  visited: var HashSet[Bag],
+  target: Bag,
+  )=
+  
+  for parentColor, subBags in table:
+    let cb = Bag(color: parentColor)
+    if cb notin visited and target in subBags:
+      visited.incl cb
+      isParentOf(table, visited, cb)
+
 block part1:
-  const myColor = "shiny gold"
-  discard
+  var visited: HashSet[Bag]
+  isParentOf(rules, visited, targetBag)
+  echo visited.len
+
+func countSubBags(
+  table: var Table[string, HashSet[Bag]], 
+  target: Bag
+  ): int=
+
+  var c = 0
+  for subBag in table[target.color]:
+    c += countSubBags(table, subBag)
+
+  target.count * (c+1)
 
 block part2:
-  discard
+  echo countSubBags(rules, targetBag) - 1
