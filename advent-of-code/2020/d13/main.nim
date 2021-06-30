8aa48c190f3c2eaf678260ddc82f9e0a35071849
hamidb80 <hr.bolouri@gmail.com>
2021-06-30 10:32:07 +0430

    finally

--- advent-of-code/2020/d13/main.nim
+++ advent-of-code/2020/d13/main.nim
@@ -1,8 +1,8 @@
-import strutils, sequtils, math, sugar
+import strutils, sequtils, sugar
 import print
 
 let 
-  lines = (readFile "./sample.txt").splitLines
+  lines = (readFile "./input.txt").splitLines
   tttbs  = lines[0].parseInt # time to take to bus top
 
 
@@ -14,16 +14,26 @@ block part1:
 
   echo busList[minWaitIndex] * waits[minWaitIndex]
 
-func occur(base, divid, plus, step: int): int=
+func occur(base, divid, plus, step: int, includeStart: bool): int=
   if divid == 0: return base
   var time = base
 
-  while (time + plus) mod divid != 0:
+  debugEcho "occur ", (base, divid, plus, step, includeStart)
+
+  while true:
+    if (time + plus) mod divid == 0:
+      if time == base:
+        if includeStart:
+          break
+      else:
+        break
+
     time += step
 
   time
 
-proc drawTimeline(time: int, busIds:seq[int])=
+# for debug purpose
+proc drawTimeline(time: int, busIds:seq[int], `for`: int)=
   let 
     off = busIds.mapIt:
       let t = time mod it
@@ -32,7 +42,7 @@ proc drawTimeline(time: int, busIds:seq[int])=
     max = max off
 
 
-  for timeCursor in time..time+max:
+  for timeCursor in time..time+`for`:
     let a = collect newseq:
       for it in busIds:
         if timeCursor mod it == 0: '0'
@@ -43,9 +53,6 @@ proc drawTimeline(time: int, busIds:seq[int])=
 
 
 block part2:
-  # print occur(17, 13, +2)
-  # print occur(13, 17, -2)
-
   let 
     busTimingList = lines[1].split(',').mapIt:
       if it == "x": 0
@@ -53,17 +60,18 @@ block part2:
 
   print busTimingList
 
-  var ans = busTimingList[0]
+  var 
+    ans = busTimingList[0]
+    step = busTimingList[0]
+  
   for i in 1..busTimingList.high:
     let id = busTimingList[i]
 
     if id == 0: 
       continue
     
-    let step = ans * (busTimingList[i-1] + 1)
-
-    echo "occur", (ans, id, i, step)
-    ans = occur(ans, id, i, step)
+    ans = occur(ans, id, i, step, true)
+    step = occur(ans, id, i, step, false) - ans
 
   echo ans
-  drawTimeline ans, busTimingList.filterIt it != 0
+  # drawTimeline ans, busTimingList.filterIt it != 0, 20
