c7f897243031db9403edc68dec5d796d6b2dfbfd
hamidb80 <hr.bolouri@gmail.com>
2021-07-02 07:19:31 +0430

    pretty

--- advent-of-code/2020/d15/main.nim
+++ advent-of-code/2020/d15/main.nim
@@ -5,29 +5,29 @@ import strutils, sequtils, tables, sugar
 
 const NotFound = 0
 
-type 
+type
   ## number: [lastIndexSeen, secondsToLastIndexSeen]
   NumberIndexTable = Table[int, array[2, int]]
 
 # functionalities ----------------------------------------------
 
-proc nth(nimap: NumberIndexTable, until, startWith: int): int=
-  var 
+proc nth(nimap: NumberIndexTable, until, startWith: int): int =
+  var
     myMap = nimap
     lsatSpokenNumber = startWith
 
   for i in myMap.len+1..until:
     # print lsatSpokenNumber
 
-    let 
-      m = myMap[lsatSpokenNumber] # [3, 0]
+    let
+      m = myMap[lsatSpokenNumber]
       n =
         if m[1] == NotFound: 0
         else: m[0] - m[1]
 
     # print m
     # print n
-    
+
     myMap[n] =
       if myMap.hasKey n: [i, myMap[n][0]]
       else: [i, NotFound]
@@ -36,21 +36,22 @@ proc nth(nimap: NumberIndexTable, until, startWith: int): int=
 
     # print myMap
     # echo "------------"
-    
-  lsatSpokenNumber
 
-# print nimap
-# echo nth(nimap, 10, lastNum)
+  lsatSpokenNumber
 
 # preparing data --------------------------------------------
 
-let 
+let
   nums = readFile("./input.txt").split(",").map parseInt
   lastNum = nums[^1]
   nimap = collect initTable: # number->index map
-    for i,n in nums:
-      {n: [i+1,NotFound]}
+    for i, n in nums:
+      {n: [i+1, NotFound]}
 
+# code -----------------------------------------------------
+
+# print nimap
+# echo nth(nimap, 10, lastNum)
 
 block part1:
   echo nth(nimap, 2020, lastNum)
