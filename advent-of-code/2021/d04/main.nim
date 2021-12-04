79b57d781cca5863594f2cc68229ba6b8de0cc98
hamidb80 <hr.bolouri@gmail.com>
2021-12-04 14:06:49 +0330

    polish

--- advent-of-code/2021/d04/main.nim
+++ advent-of-code/2021/d04/main.nim
@@ -6,6 +6,7 @@ type Board = seq[int] # with cap of 25
 const
   pin = -1
   notFound = -1
+  rowOffsets = countup(0, 25 - 1, 5).toseq
 
 func extractInfo(body: string): tuple[header: seq[int], boards: seq[Board]] =
   let i = body.find("\n")
@@ -15,13 +16,12 @@ func extractInfo(body: string): tuple[header: seq[int], boards: seq[Board]] =
   for board_offset in countup(0, numbers.high, 25):
     result.boards.add numbers[board_offset..(board_offset + 25 - 1)]
 
-func spaced(n, desiredLen: int): string=
+func spaced(n, desiredLen: int): string =
   let s = $n
   repeat(' ', desiredLen - s.len) & s
 
-func `$`(b: Board): string=
-  countup(0, 25 - 1, 5)
-  .toseq
+func `$`(b: Board): string = ## for debugging purposes
+  rowOffsets
   .mapit(b[it..(it+4)].mapIt(it.spaced 2).join" ")
   .join "\n"
 
@@ -36,14 +36,14 @@ func pinNumber(boards: var seq[Board], num: int) =
 
 func checkWin(board: Board): bool =
   # rows check
-  for rowOffset in countup(0, 25 - 1, 5):
+  for rowOffset in rowOffsets:
     if board[rowOffset..(rowOffset + 5 - 1)].allIt it == pin:
       # debugecho "row checked"
       return true
 
   # columns check
   for col_i in 0..<5:
-    if countup(0, 25 - 1, 5).toseq.allIt(board[it + col_i] == pin):
+    if rowOffsets.allIt(board[it + col_i] == pin):
       # debugecho "col checked"
       return true
 
@@ -77,14 +77,14 @@ func test2(header: seq[int], boards: seq[Board]): int =
     pinnumber myboards, n
     if i < 5: continue
 
-    if myboards.len == 1:
+    if myboards.len == 1: ## special care for last one
       if myboards[0].checkwin:
         # debugecho myboards[0]
         return myboards[0].winnerSum * n
-      
+
     else:
       myboards.keepitif not it.checkwin
-      
+
 # run -------------------------
 
 let content = readfile("./input.txt").extractInfo
