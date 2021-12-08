b212807d65e633db45a9a92cde9019434762876d
hamidb80 <hr.bolouri@gmail.com>
2021-12-08 15:11:42 +0330

    typo

--- advent-of-code/2021/d08/main.nim
+++ advent-of-code/2021/d08/main.nim
@@ -32,15 +32,13 @@ const
 
   uniqLens = [1, 4, 7, 8].mapIt(numberSegments[it].len)
 
-
-
 # utils --------------------------------------
 
 func parseLine(line: string): Input =
   let s = line.split('|').mapIt it.strip.splitWhitespace
   (s[0].mapit it.toHashSet, s[1].mapIt(it.toHashSet))
 
-func getOne[T](s: HashSet[T]): T=
+func first[T](s: HashSet[T]): T=
   for i in s:
     return i
 
@@ -54,23 +52,22 @@ func extractSpecials(signalPatterns: seq[Pattern]): seq[Pattern] {.inline.}=
   signalPatterns.filterIt(it.len in uniqLens).sorted do (s1, s2: Pattern) -> int:
     cmp s1.len, s2.len
 
-func detectWireTable(patterns: seq[Pattern]): WireTable =
+func resolveWireTable(patterns: seq[Pattern]): WireTable =
   # w: wire, ws: wires
   # p: pattern
   let 
     specials = extractSpecials patterns
     ws_cf = specials[wi1]
-    ws_bcdf = specials[wi4]
     ws_acf = specials[wi7]
     all = specials[wi8]
-    
-    w_a = ws_acf - ws_cf
 
     ps_069 = patterns.filterIt(it.len == 6)
     ps_235 = patterns.filterIt(it.len == 5)
     ws_abfg = ps_069.foldl a * b
     ws_adg = ps_235.foldl a * b
     ws_cde = all - ws_abfg
+
+    w_a = ws_acf - ws_cf
     w_d = ws_adg - ws_abfg
     w_f = ws_cf - ws_cde
     w_c = ws_cf - w_f
@@ -79,13 +76,13 @@ func detectWireTable(patterns: seq[Pattern]): WireTable =
     w_g = ws_adg - (w_a + w_d)
 
   totable {
-    getOne w_a: 'a',
-    getOne w_b: 'b',
-    getOne w_c: 'c',
-    getOne w_d: 'd',
-    getOne w_e: 'e',
-    getOne w_f: 'f',
-    getOne w_g: 'g',
+    first w_a: 'a',
+    first w_b: 'b',
+    first w_c: 'c',
+    first w_d: 'd',
+    first w_e: 'e',
+    first w_f: 'f',
+    first w_g: 'g',
   }
 
 func resolveDigit(digit: Pattern): int=
@@ -104,7 +101,7 @@ func genNumber(digits: seq[Pattern]): int=
 
 func sumOutputs(data: seq[Input]): int =
   sum data.mapIt do:
-    let wt = it.signalPatterns.detectWireTable
+    let wt = it.signalPatterns.resolveWireTable
     it.output.mapIt(it.transformSegs wt).genNumber()
 
 # go -----------------------------------------
