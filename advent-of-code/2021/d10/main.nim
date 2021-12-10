040f3b4a0b6d09e05a12343f47819cf7d68044cc
hamidb80 <hr.bolouri@gmail.com>
2021-12-10 09:15:52 +0330

    add aoc/2021/d10

---
+++ advent-of-code/2021/d10/main.nim
@@ -0,0 +1,53 @@
+import sequtils, tables, algorithm
+
+# prepare ------------------------------------
+
+const
+  scoreMap1 = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137,
+  }.toTable
+
+  opens = ['(', '[', '{', '<']
+  closes = [')', ']', '}', '>']
+  openCloseMap = zip(opens, closes).toTable
+
+# utils --------------------------------------
+
+# implement ----------------------------------
+
+func syntaxErrorScore(lines: seq[string], incomplete: static bool): int =
+  var scores: seq[int]
+
+  for l in lines:
+    var stack: seq[char]
+    for c in l:
+      if c in opens:
+        stack.add openCloseMap[c]
+      elif c == stack[^1]:
+        stack.del stack.high
+      else:
+        when incomplete:
+          stack = @[]
+        else:
+          result.inc scoreMap1[c]
+        break
+
+    when incomplete:
+      if stack.len != 0:
+        scores.add 0
+
+        for (i, n) in stack.mapIt(closes.find(it) + 1).reversed.pairs:
+          scores[^1] = scores[^1] * 5 + n
+
+  when incomplete:
+    scores.sorted[scores.len div 2]
+
+
+# go -----------------------------------------
+
+let content = lines("./input.txt").toseq
+echo syntaxErrorScore(content, false)
+echo syntaxErrorScore(content, true)
