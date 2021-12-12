fed604b5101fa3cfa046ee78cee0801569b1ff62
hamidb80 <hr.bolouri@gmail.com>
2021-12-12 09:52:54 +0330

    add aoc/2021/d12/p2

--- advent-of-code/2021/d12/main.nim
+++ advent-of-code/2021/d12/main.nim
@@ -7,9 +7,10 @@ type
   Connection = array[2, Cave]
   Path = seq[Cave]
 
-const 
+const
   startCave = "start".Cave
   endCave = "end".Cave
+  noCave = "".Cave
 
 proc parseInput(fname: string): seq[Connection] =
   for l in lines fname:
@@ -19,11 +20,14 @@ proc parseInput(fname: string): seq[Connection] =
 
 # utils --------------------------------------
 
-proc `[]`(c: Cave, index: int): char = c.string[index]
-proc `$`(c: Cave): string = c.string
-proc `==`(c1, c2: Cave): bool {.borrow.}
+func `==`(c1, c2: Cave): bool {.borrow.}
+func `[]`(c: Cave, index: int): char = c.string[index]
+func `$`(c: Cave): string = c.string ## debuggin purposes
 
 func isLarge(c: Cave): bool = (c[0].ord - 'a'.ord) < 0
+
+template concat[T](head: seq[T], tail: T): untyped =
+  head.concat(@[tail])
 
 # implement ----------------------------------
 
@@ -33,31 +37,40 @@ func findConnectionsFor(connections: seq[Connection], `for`: Cave): seq[Cave] =
       result.add conn.filterIt(it != `for`)[0]
 
 func findPathImpl(
-  connections: seq[Connection], `from`: Cave, 
-  currentPath: seq[Cave], paths: var seq[Path]
+  connections: seq[Connection], `from`: Cave,
+  currentPath: seq[Cave], paths: var seq[Path],
+  escapedCave: Cave
 ) =
   for cave in findConnectionsFor(connections, `from`):
     if cave == startCave:
       continue
-    
+
     elif cave == endCave:
-      paths.add currentPath.concat(@[endCave])
-    
+      paths.add currentPath.concat(endCave)
+
     else:
       let visitedBefore = cave in currentPath
-      
-      if (cave.isLarge and visitedBefore) or (not visitedBefore):
-        findPathImpl connections, cave, currentPath.concat(@[cave]), paths
 
-func findPath(connections: seq[Connection]): seq[Path] =
-  findPathImpl(connections, startCave, @[startCave], result)
+      template recall(ec): untyped =
+        findPathImpl connections, cave, currentPath.concat(cave), paths, ec
 
-func howManyWays(content: seq[Connection]): int =
-  let tmp = findPath content
-  # debugEcho tmp
-  len tmp
+      if (not visitedBefore) or (cave.isLarge and visitedBefore):
+        recall escapedCave
+      elif escapedCave == noCave:
+        recall cave
+
+func findPath(connections: seq[Connection], canVisitTwice: static bool): seq[Path] =
+  const escapedCave =
+    if canVisitTwice: noCave
+    else: startCave
+
+  findPathImpl(connections, startCave, @[startCave], result, escapedCave)
+
+func howManyWays(content: seq[Connection], singleTwice: static bool): int =
+  len findPath(content, singleTwice)
 
 # go -----------------------------------------
 
 let content = parseInput("./input.txt")
-echo howManyWays(content) # 3495
+echo howManyWays(content, false) # 3495
+echo howManyWays(content, true) # 94849
