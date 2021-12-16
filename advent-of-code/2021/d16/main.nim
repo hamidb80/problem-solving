a4e5dcf541791b71c816427b6ffce91740ba767c
hamidb80 <hr.bolouri@gmail.com>
2021-12-16 13:27:42 +0330

    p1

--- advent-of-code/2021/d16/main.nim
+++ advent-of-code/2021/d16/main.nim
@@ -1,4 +1,4 @@
-import sequtils, strutils, strformat
+import sequtils, strutils
 
 # prepare ------------------------------------
 
@@ -14,7 +14,7 @@ type
     version, typeId: int
 
     case kind: PacketKind
-    of PkLiteral: value: int 
+    of PkLiteral: value: int
     of PkOperator: nodes: seq[DataPacket]
 
   ParseStates = enum
@@ -28,16 +28,23 @@ template isAvailable(v: int): untyped =
 
 # utils --------------------------------------
 
+func countLeading(str: string, chr: char): int=
+  for c in str:
+    if c == chr:
+      result.inc
+    else:
+      break
+
 func parseInput(s: sink string): RawPacket =
   result = new RawPacket
   result[] = newStringOfCap(s.len * 4)
 
-  for n in countup(4, s.len, 4):
-    result[] &= s[n-4 ..< n].parseHexInt.toBin(4)
+  for c in s:
+    result[] &= ($c).parseHexInt.toBin(4)
 
-func parseLiteralBin(s: string): int=
-  var acc:string
-  
+func parseLiteralBin(s: string): int =
+  var acc: string
+
   for i in countup(5, s.len, 5):
     acc.add s[(i-4) ..< i]
 
@@ -45,7 +52,8 @@ func parseLiteralBin(s: string): int=
 
 # implement ----------------------------------
 
-func parseRawPacketImpl(rp: RawPacket, rng: HSlice[int, int], acc: var DataPacket): int =
+func parseRawPacketImpl(rp: RawPacket, rng: HSlice[int, int],
+    acc: var DataPacket): int =
   var
     i = rng.a
     numberOfSubPackets = notAvailable
@@ -53,20 +61,13 @@ func parseRawPacketImpl(rp: RawPacket, rng: HSlice[int, int], acc: var DataPacke
     state = PsVersion
     myversion = notAvailable
 
-  template at(n: int): untyped= rp[][n]
+  template at(n: int): untyped = rp[][n]
   template resolve(`from`, size: int): untyped =
-    if acc != nil:
-      debugEcho fmt"{acc.typeId = }"
-      debugEcho fmt"{acc.version = }"
-
-    debugEcho `from` ..< (`from` + size), " of ", rp[].len, " >> ", state
-    debugEcho rp[][`from` ..< (`from` + size)]
-    debugEcho "-----------"
-
     rp[][`from` ..< (`from` + size)].parseBinInt
 
-  while (i <= rng.b) and (state != PsEnd):
+  while i <= rng.b:
     case state:
+    of PsEnd: break
     of PsVersion:
       myversion = resolve(i, 3)
       state = PsTypeId
@@ -102,10 +103,10 @@ func parseRawPacketImpl(rp: RawPacket, rng: HSlice[int, int], acc: var DataPacke
       i.inc 1
 
       if lenTid == '0':
-        numberOfSubPackets = resolve(i, 15) 
+        totalLengthOfSubPackets = resolve(i, 15)
         i.inc 15
       else:
-        totalLengthOfSubPackets = resolve(i, 11)
+        numberOfSubPackets = resolve(i, 11)
         i.inc 11
 
       state = PsSubPackets
@@ -113,12 +114,12 @@ func parseRawPacketImpl(rp: RawPacket, rng: HSlice[int, int], acc: var DataPacke
     of PsSubPackets:
       if isAvailable numberOfSubPackets:
         var c = 0
-        while (c < numberOfSubPackets) and (i <= rng.b):
+        while (c != numberOfSubPackets) and (i <= rng.b):
           var myPacket: DataPacket
           i = parseRawPacketImpl(rp, i .. rng.b, myPacket)
           acc.nodes.add myPacket
           c.inc
-      
+
       else:
         let limit = i + totalLengthOfSubPackets - 1
 
@@ -129,14 +130,12 @@ func parseRawPacketImpl(rp: RawPacket, rng: HSlice[int, int], acc: var DataPacke
 
       state = PsEnd
 
-    of PsEnd: break
-  
   i
 
-func parseRawPacket(rp: RawPacket): DataPacket=
+func parseRawPacket(rp: RawPacket): DataPacket =
   discard parseRawPacketImpl(rp, 0 .. rp[].high, result)
 
-func sumVersionsImpl(packetTree: DataPacket, acc: var int)=
+func sumVersionsImpl(packetTree: DataPacket, acc: var int) =
   acc.inc packetTree.version
 
   if packetTree.kind == PkOperator:
@@ -146,7 +145,10 @@ func sumVersionsImpl(packetTree: DataPacket, acc: var int)=
 func sumVersions(packetTree: DataPacket): int =
   sumVersionsImpl(packetTree, result)
 
+# func sumVersionsImpl(packetTree: DataPacket, acc: var int) =
+# func sumVersions(packetTree: DataPacket): int =
+
 # go -----------------------------------------
 
-let data = readFile("./test1.txt").parseInput
-echo sumVersions data.parseRawPacket
+let data = readFile("./input.txt").parseInput
+echo sumVersions data.parseRawPacket # 984
