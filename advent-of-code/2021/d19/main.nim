e515facb1d8456708b56b658320b1994d719fbd2
hamidb80 <hr.bolouri@gmail.com>
2021-12-20 20:57:22 +0330

    p1

--- advent-of-code/2021/d19/main.nim
+++ advent-of-code/2021/d19/main.nim
@@ -1,24 +1,29 @@
-import std/[sequtils, strutils, strscans, math, tables, intsets, unittest]
+import std/[sequtils, strutils, strscans, math, tables, intsets, unittest, strformat]
 
 {.experimental: "strictFuncs".}
 
 # def ------------------------------------
 
 type
-  Point* = object
-    x, y, z: int
-
   Axises = enum
     X, Y, Z
 
-  DirectionComparision = tuple[sameSign: bool, axis: Axises]
+  Point* = object
+    x, y, z: int
+
+  Vector = Point
+
+  DirectionComparision = tuple
+    sameSign: bool
+    axis: Axises
 
   Rotation = object
     x, y, z: DirectionComparision
 
-  Transform = tuple
-    position: Point
+  Transform = object
+    movement: Point
     rotation: Rotation
+    reversed: bool
 
   Scanner = object
     id: int
@@ -29,15 +34,34 @@ type
     transform: Transform
 
   RelationTable = Table[int, seq[Relation]]
-  TransformTable = seq[seq[Transform]]
+  TransformTable = Table[int, seq[Transform]]
 
 # utils --------------------------------------
+
+template findIt(s, cond: untyped): untyped =
+  var
+    temp: typeof s[0]
+    found = false
+
+  for it {.inject.} in s:
+    if cond:
+      temp = it
+      found = true
+      break
+
+  if not found:
+    raise newException(ValueError, "not found")
+
+  temp
 
 func parsePoint(s: sink string): Point =
   discard scanf(s, "$i,$i,$i", result.x, result.y, result.z)
 
 func parseHeader(s: sink string): int =
   discard scanf(s, "--- scanner $i ---", result)
+
+func newTransform(m: Vector, ro: Rotation, rev: bool = false): Transform =
+  Transform(movement: m, rotation: ro, reversed: rev)
 
 func parseInput(s: sink string): seq[Scanner] =
   for sc in s.split "\r\n\r\n":
@@ -112,52 +136,47 @@ func resolveSign(sameSign: bool): int =
   if sameSign: +1
   else: -1
 
-func add(acc: var RelationTable, id: int, rel: Relation) =
-  if id in acc:
-    acc[id].add rel
+func addto[K, V](acc: var Table[K, seq[V]], key: K, val: V) =
+  if key in acc:
+    acc[key].add val
   else:
-    acc[id] = @[rel]
+    acc[key] = @[val]
 
 func rotate(p: Point, r: Rotation): Point =
   for ax in [X, Y, Z]:
-    result[ax] = p[r[ax].axis] * resolveSign(r[ax].sameSign)
+    result[r[ax].axis] = p[ax] * resolveSign(r[ax].sameSign)
 
 func `^`(r: Rotation): Rotation =
   for ax in [X, Y, Z]:
     result[r[ax].axis] = (r[ax].sameSign, ax)
 
 func `^`(tr: Transform): Transform =
-  (-tr.position, ^tr.rotation)
+  newTransform(tr.movement, tr.rotation, not tr.reversed)
 
 func transform(p: Point, tr: Transform): Point =
-  (tr.position + p).rotate(tr.rotation)
+  if tr.reversed:
+    (p - tr.movement).rotate(^tr.rotation)
+  else:
+    p.rotate(tr.rotation) + tr.movement
 
 func transform(p: Point, trs: seq[Transform]): Point =
   result = p
   for t in trs:
-    result = transform(p, t)
+    result = transform(result, t)
 
 func validCond(p: Point): bool =
   let s = p.toSeq
   s.allIt(it != 0) and s.deduplicate.len == 3
 
 func findRotation(dp1, dp2: Point): Rotation =
-  # debugEcho "{{{{{{{{"
-  # debugEcho dp1, dp2
-
-  assert validCond(dp1) and validCond(dp2)
-
   for ax1, v1 in dp1.pairs:
     for ax2, v2 in dp2.pairs:
       if abs(v1) == abs(v2):
         result[ax1] = (sgn(v1) == sgn(v2), ax2)
 
-  # debugEcho result
-  # debugEcho "}}}}}}}}"
-
   assert [result.x.axis, result.y.axis, result.z.axis].deduplicate.len == 3
 
-func findTransformationPath(rels: RelationTable, path: seq[int], dest: int,
+func findTransformationPathImpl(rels: RelationTable, path: seq[int], dest: int,
     result: var seq[int]
 ) =
   for r in rels[path[^1]]:
@@ -168,30 +187,37 @@ func findTransformationPath(rels: RelationTable, path: seq[int], dest: int,
       return
 
     elif r.with notin path:
-      findTransformationPath(rels, newp, dest, result)
+      findTransformationPathImpl(rels, newp, dest, result)
+
+func findTransformationPath(rels: RelationTable, `from`, to: int): seq[int] =
+  findTransformationPathImpl(rels, @[`from`], to, result)
+
+func findMove(p1, p2: Point, ro: Rotation): Vector =
+  ## p1 and p2 are in the same position but in different coordinate system
+  p2 - p1.rotate(ro)
 
 func genTransformTable(relTable: RelationTable, `from`: int): TransformTable =
-  result = newSeqWith(relTable.len, newseq[Transform]())
-
-  for id in 0 ..< relTable.len:
+  for id in relTable.keys:
     if id == `from`: continue
 
-    var path: seq[int]
-    findTransformationPath(relTable, @[`from`], id, path)
+    let path = findTransformationPath(relTable, id, `from`)[1..^1]
+    var head = id
+    for i in path:
+      result.addto id, (relTable[head].findIt it.with == i).transform
+      head = i
 
-    var rels = relTable[0]
-    for i in path[1..^1]:
-      for r in rels:
-        if r.with == i:
-          result[id].add r.transform
-          break
-
-      rels = relTable[i]
-
-    assert result[id].len == (path.len - 1)
+    assert result[id].len == path.len
 
 func relDistance*(pin: Point, sp: seq[Point]): IntSet =
   toIntSet sp.mapIt distance2(pin, it)
+
+func buildTransform(pin1, p1, pin2, p2: Point): Transform =
+  let
+    dp1 = pin1 - p1
+    dp2 = pin2 - p2
+    ro = findRotation(dp1, dp2)
+
+  newTransform(findMove(pin1, pin2, ro), ro)
 
 func haveInCommon(s1, s2: Scanner, atLeast: int
 ): tuple[result: bool, transform: Transform] =
@@ -212,13 +238,8 @@ func haveInCommon(s1, s2: Scanner, atLeast: int
               d1 = distance2(pin1, p1)
               d2 = distance2(pin2, p2)
 
-            if d1 == d2 and d1 != 0:
-              # debugEcho s1.id, " <-> ", s2.id
-              let 
-                dp1 = pin1 - p1
-                dp2 = pin2 - p2
-              
-              return (true, (pin1 - pin2 , findRotation(dp1, dp2)))
+            if d1 == d2 and d1 != 0 and [pin1-p1, pin2-p2].allIt(it.validCond):
+              return (true, buildTransform(pin1, p1, pin2, p2))
 
 func howManyBeacons(reports: seq[Scanner]): int =
   var
@@ -228,8 +249,8 @@ func howManyBeacons(reports: seq[Scanner]): int =
   for r1, r2 in reports.combinations:
     let cm = haveInCommon(r1, r2, 12)
     if cm.result:
-      relations.add r1.id, (r2.id, cm.transform)
-      relations.add r2.id, (r1.id, ^cm.transform)
+      relations.addto r1.id, (r2.id, cm.transform)
+      relations.addto r2.id, (r1.id, ^cm.transform)
 
   assert relations.len >= reports.len
 
@@ -237,35 +258,77 @@ func howManyBeacons(reports: seq[Scanner]): int =
     acc.add p
 
   let trTable = genTransformTable(relations, 0)
-  # debugEcho "\n<< ", trTable.join "\n<< "
 
   for id in 1..reports.high:
     acc.add reports[id].records.mapIt(transform(it, trTable[id]))
 
-  acc.deduplicate.len
+  let ps = acc.deduplicate
+  ps.len
 
 # go -----------------------------------------
 
-let data = readfile("./test.txt").parseInput
+let data = readfile("./input.txt").parseInput
 echo howManyBeacons(data)
 
+# test ------------------------------
 
-# echo "++++++++++++++++++="
-# suite "transform":
-#   test "1":
-#     let
-#       p1 = newPoint(1, 2, 3)
-#       p2 = newPoint(2, 1, -3)
-#       dp = newPoint(1, 0, 0)
-#       ro = findRotation(p1, p2)
-#       tr = (dp, ro)
-#       p3 = p2 + dp
-#       p4 = p1.transform tr
+suite "transform":
+  let
+    ps1 = [
+      newPoint(0, 2, 0),
+      newPoint(4, 1, 0),
+      newPoint(3, 3, 0),
+    ]
+    ps2 = [
+      newPoint(0, -5, 0),
+      newPoint(1, -1, 0),
+      newPoint(-1, -2, 0),
+    ]
+    ps3 = [
+      newPoint(-3, 4, 0),
+      newPoint(-4, 0, 0),
+      newPoint(-2, 1, 0),
+    ]
 
-#     echo "1. ", p1
-#     echo "2. ", p2
-#     echo "3. ", p3
-#     echo "4. ", p4
-#     echo "<> ", tr
+  test "1":
+    let tr = buildTransform(ps1[0], ps1[1], ps2[0], ps2[1])
 
-#     check p4.transform(^tr) == p1
+    check ps1[1].transform(tr) == ps2[1]
+    check ps2[1].transform(^tr) == ps1[1]
+    check ps1[1].transform( ^ ^tr) == ps2[1]
+
+  let
+    `T(1->2)` = buildTransform(ps1[0], ps1[1], ps2[0], ps2[1])
+    `T(2->3)` = buildTransform(ps2[0], ps2[1], ps3[0], ps3[1])
+
+  test "chain":
+    let
+      cp =
+        ps1[0]
+        .transform(`T(1->2)`)
+        .transform(`T(2->3)`)
+
+      rcp =
+        ps3[0]
+        .transform( ^ `T(2->3)`)
+        .transform( ^ `T(1->2)`)
+
+    check cp == ps3[0]
+    check rcp == ps1[0]
+
+  var relt: RelationTable
+  relt.addto(1, (2, `T(1->2)`))
+  relt.addto(2, (3, `T(2->3)`))
+  relt.addto(2, (1, ^`T(1->2)`))
+  relt.addto(3, (2, ^`T(2->3)`))
+
+  test "findTransformationPath":
+    check:
+      findTransformationPath(relt, 1, 2) == @[1, 2]
+      findTransformationPath(relt, 1, 3) == @[1, 2, 3]
+      findTransformationPath(relt, 2, 3) == @[2, 3]
+      findTransformationPath(relt, 3, 1) == @[3, 2, 1]
+
+  test "genTransformTable":
+    let tts = genTransformTable(relt, 1)
+    check ps3[0].transform(tts[3]) == ps1[0]
