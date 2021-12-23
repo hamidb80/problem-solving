1c0ae5308eb67065112d5c409c59a3a56f671ac9
hamidb80 <hr.bolouri@gmail.com>
2021-12-23 15:30:36 +0330

    i

--- advent-of-code/2021/d23/main.nim
+++ advent-of-code/2021/d23/main.nim
@@ -1,14 +1,42 @@
-import std/[sequtils, strutils, strformat, unittest]
+import std/[sequtils, strutils, strformat, unittest, tables]
 
 {.experimental: "strictFuncs".}
 
 # def ----------------------------------------
 
+type
+  CellState = enum
+    Empty = '.'
+    A = 'A'
+    B = 'B'
+    C = 'C'
+    D = 'D'
+
+  Highway = array[11, CellState]
+  Room = array[2, CellState]
+  Burrow = array[4, Room]
+
+const
+  roomEntries = [2, 4, 6, 8]
+  amphipodOrder = [A, B, C, D]
+  energyCost = {A: 1, B: 10, C: 100, D: 1000}.toTable
+
 # utils --------------------------------------
+
+func parseInput(s: sink string): Burrow =
+  let rows =
+    s.splitLines[2..3].mapit do:
+      it[3..9]
+      .split('#')
+      .mapIt parseEnum[CellState](it)
+
+  for c in 0 ..< 4:
+    for r in 0 ..< 2:
+      result[c][r] = rows[r][c]
 
 # implement ----------------------------------
 
-func test(data: string): int =
+func leastEnergyToArrange(burrow: Burrow): int =
   discard
 
 # tests --------------------------------------
@@ -18,5 +46,6 @@ test "":
 
 # go -----------------------------------------
 
-let data = readFile("./test.txt")
-echo test(data)
+let data = readFile("./test.txt").parseInput
+echo data
+# echo test(data)
