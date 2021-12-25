a31c9a458cc8f4d145c149dcfddd94c97220a6f7
hamidb80 <hr.bolouri@gmail.com>
2021-12-25 19:29:22 +0330

    thinking

--- advent-of-code/2021/d23/main.nim
+++ advent-of-code/2021/d23/main.nim
@@ -18,6 +18,7 @@ type
 
 const
   roomEntries = [2, 4, 6, 8]
+  allowedStops = (0..<11).toseq.filterit it notin roomEntries
   amphipodOrder = [A, B, C, D]
   energyCost = {A: 1, B: 10, C: 100, D: 1000}.toTable
 
@@ -34,9 +35,13 @@ func parseInput(s: sink string): Burrow =
     for r in 0 ..< 2:
       result[c][r] = rows[r][c]
 
-func toHighway(s: string = ""): Highway=
-  for (i,c ) in s.pairs:
+func initHighway(s: string = ""): Highway =
+  for (i, c) in s.pairs:
     result[i] = parseEnum[CellState]($c)
+
+func replace[N, T](a: array[N, T], index: int, val: T): array[N, T] =
+  result = a
+  result[index] = val
 
 # implement ----------------------------------
 
@@ -47,7 +52,7 @@ func render(b: Burrow, h: Highway): string =
   "  #" & b.mapIt(it[1]).join"#" & "#\n" &
   "  #########"
 
-func isFinished(burrow: Burrow, highway: Highway): bool=
+func isFinished(burrow: Burrow, highway: Highway): bool =
   if highway.allIt it == Empty:
     for i, a in amphipodOrder.pairs:
       if not burrow[i].allIt(it == a):
@@ -56,19 +61,37 @@ func isFinished(burrow: Burrow, highway: Highway): bool=
     true
   else: false
 
-const notFound = -1
-func leastEnergyToArrangeImpl(burrow: Burrow, highway: Highway, result: var int)=
-  var 
-    mb = burrow
-    mh  = highway
+func canGotoRoom(r: Room, amphipodType: CellState): tuple[can: bool, depth: int] =
+  if r[0] == Empty:
+    if r[1] == Empty:
+      (true, 2)
+    elif r[1] == amphipodType:
+      (true, 1)
+    else:
+      (false, 0)
+
+  else:
+    (false, 0)
+
+func leastEnergyToArrange(burrow: Burrow): int =
+  var highway = initHighway()
+
+  # check any of them can goto the home
+  for i, cell in highway.pairs:
+    if cell != Empty:
+      # check inside it's destination room
+      let 
+        ri = amphipodOrder.find(cell) # room index
+        (can, depth) = canGotoRoom(burrow[ri], cell)
+      
+      if can:
+        discard
 
   for room in burrow:
-    for hi in 0 .. highway.high:
-      discard
-  
-func leastEnergyToArrange(burrow: Burrow): int =
-  leastEnergyToArrangeImpl(burrow, toHighway(), result)
-
+    for stp in allowedStops:
+      if highway[stp] == Empty:
+        discard
+       
 # tests --------------------------------------
 
 # test "":
