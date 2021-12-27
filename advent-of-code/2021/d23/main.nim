7634c50a2faefd3cf40229c715878891230e1c19
hamidb80 <hr.bolouri@gmail.com>
2021-12-27 22:24:19 +0330

    it's working but it's super sssslllllloooooooowwwwww

--- advent-of-code/2021/d23/main.nim
+++ advent-of-code/2021/d23/main.nim
@@ -1,4 +1,4 @@
-import std/[sequtils, strutils, strformat, unittest, tables, math]
+import std/[sequtils, strutils, unittest, tables]
 
 {.experimental: "strictFuncs".}
 
@@ -16,6 +16,8 @@ type
   Room = array[2, CellState]
   Burrow = array[4, Room]
 
+  Data = tuple[highway: Highway, burrow: Burrow]
+
 const
   roomEntries = [2, 4, 6, 8]
   allowedStops = (0..<11).toseq.filterit it notin roomEntries
@@ -24,7 +26,11 @@ const
 
 # utils --------------------------------------
 
-func parseInput(s: sink string): Burrow =
+func parseHighway(s: sink string): Highway =
+  for i, c in s.pairs:
+    result[i] = parseEnum[CellState]($c)
+
+func parseInput(s: sink string): Data =
   let rows =
     s.splitLines[2..3].mapit do:
       it[3..9]
@@ -33,7 +39,9 @@ func parseInput(s: sink string): Burrow =
 
   for c in 0 ..< 4:
     for r in 0 ..< 2:
-      result[c][r] = rows[r][c]
+      result.burrow[c][r] = rows[r][c]
+
+  result.highway = parseHighway s.splitLines[1][1 .. ^2]
 
 func replace[N, T](a: array[N, T], index: int, val: T): array[N, T] =
   result = a
@@ -53,18 +61,20 @@ func render(b: Burrow, h: Highway): string =
   "  #########"
 
 func isSolved(burrow: Burrow): bool =
-  for i, a in amphipodOrder.pairs:
-    if not burrow[i].allIt(it == a):
+  result = true
+  for i, owner in amphipodOrder.pairs:
+    if not burrow[i].allIt(it == owner):
       return false
 
-func isRoadFree(`from`, to: int, h: Highway): bool =
-  let road =
-    if `from` <= to:
-      h[`from` .. to]
-    else:
-      h[to .. `from`]
+func correctOrder(rng: HSlice[int, int], startOffset = 0): HSlice[int, int] =
+  if rng.a > rng.b:
+    rng.b .. rng.a-startOffset
+  else:
+    rng.a+startOffset .. rng.b
 
-  road.allIt it == Empty
+func isRoadFree(head, tail: int, h: Highway): bool =
+  let road = correctOrder(head .. tail, 1)
+  h[road].allIt it == Empty
 
 func canEnterInRoom(r: Room, owner: CellState): tuple[can: bool, depth: int] =
   if r[0] == Empty:
@@ -89,19 +99,21 @@ func exportMember(r: Room, owner: CellState): tuple[should: bool, index: int] =
       result = (true, 0)
 
   else:
-    if r[0] != owner:
+    if r[0] != Empty and r[0] != owner:
       result = (true, 0)
 
 func leastEnergyImpl(
   burrow: Burrow, highway: Highway, costYet: int
 ): tuple[solved: bool, totalCost: int] =
+  result.totalCost = int.high
 
   var didSomeOperation = false
   template wellDone: untyped =
     didSomeOperation = true
 
   template reCalculate(res): untyped =
-    result = (true, min(result.totalCost, res.totalCost))
+    if res.solved:
+      result = (true, min(result.totalCost, res.totalCost))
 
   for i, cell in highway.pairs:
     if cell != Empty:
@@ -112,54 +124,127 @@ func leastEnergyImpl(
         if can:
           wellDone()
 
+          # debugEcho "\n----------------\n"
+          # debugEcho render(burrow, highway)
+          # debugEcho costYet
+
           let
-            distance = abs(i - ri) + depth
+            distance = abs(i - roomEntries[ri]) + depth
             res = leastEnergyImpl(
               burrow.replace(ri, depth - 1, cell),
               highway.replace(i, Empty),
               costyet + distance * energyCost[ri]
             )
 
-          if res.solved:
-            reCalculate res
+          reCalculate res
 
   for stp in allowedStops:
-    let cell = highway[stp]
-
-    if cell == Empty:
+    if highway[stp] == Empty:
       for ri, room in burrow.pairs:
-        if not isRoadFree(roomEntries[ri], stp, highway):
-          continue
+        if isRoadFree(roomEntries[ri], stp, highway) and not room.isEmpty:
+          let
+            (should, i) = exportMember(room, amphipodOrder[ri])
+            xdistance = abs(stp - roomEntries[ri])
 
-        let
-          (should, i) = exportMember(room, amphipodOrder[ri])
-          distance = abs(stp - roomEntries[ri])
+          if should:
+            wellDone()
 
-        if should:
-          wellDone()
-          let res = leastEnergyImpl(
-            burrow.replace(ri, i, Empty),
-            highway.replace(stp, room[i]),
-            costyet + (i + 1 + distance) * energyCost[ri]
-          )
+            # debugEcho "\n------  --------\n"
+            # debugEcho render(burrow, highway)
+            # debugEcho costYet
 
-          if res.solved:
+            let res = leastEnergyImpl(
+              burrow.replace(ri, i, Empty),
+              highway.replace(stp, room[i]),
+              costyet + (i + 1 + xdistance) * energyCost[amphipodOrder.find room[i]]
+            )
+
             reCalculate res
 
   if not didSomeOperation:
-    result = (isSolved(burrow), costYet)
+    result = (isSolved(burrow), costyet)
 
-func leastEnergy(burrow: Burrow, highway = Highway.default): int =
+func leastEnergy(burrow: Burrow, highway: Highway): int =
   let r = leastEnergyImpl(burrow, highway, 0)
   assert r.solved
   r.totalCost
 
 # tests --------------------------------------
 
-# test "":
-#   check true
+test "correctOrder":
+  check correctOrder(2 .. 5) == 2..5
+  check correctOrder(5 .. 2) == 2..5
+  check correctOrder(2 .. 5, 1) == 3..5
+  check correctOrder(5 .. 2, 1) == 2..4
+
+test "isSolved":
+  check isSolved([ [A, A], [B, B], [C, C], [D, D]])
+  check not isSolved([ [B, A], [A, B], [C, C], [D, D]])
+
+test "is road free":
+  block no:
+    var hw = Highway.default
+    hw[4] = A
+    check not isRoadFree(2, 4, hw)
+    check isRoadFree(4, 2, hw)
+
+  block yes:
+    var hw = Highway.default
+    check isRoadFree(2, 4, hw)
+    check isRoadFree(4, 2, hw)
+
+suite "move":
+  test "1 steps to win":
+    let d = """
+      #############
+      #.........A.#
+      ###.#B#C#D###
+        #A#B#C#D#
+        #########
+    """.strip.unindent(6).parseInput
+    check leastEnergy(d.burrow, d.highway) == 8
+    
+  test "2 steps to win":
+    let d = """
+      #############
+      #...B.....A.#
+      ###.#.#C#D###
+        #A#B#C#D#
+        #########
+    """.strip.unindent(6).parseInput
+    check leastEnergy(d.burrow, d.highway) == 28
+
+  test "3 steps to win":
+    let d = """
+      #############
+      #.....D.D.A.#
+      ###.#B#C#.###
+        #A#B#C#.#
+        #########
+    """.strip.unindent(6).parseInput
+    check leastEnergy(d.burrow, d.highway) == (3 + 4) * 1000 + 8
+
+  test "5 steps to win":
+    let d = """
+      #############
+      #.....D.....#
+      ###B#.#C#D###
+        #A#B#C#A#
+        #########
+    """.strip.unindent(6).parseInput
+    check leastEnergy(d.burrow, d.highway) == 9011 + 40
+
+  test "8 steps to win":
+    let d = """
+      #############
+      #...B.......#
+      ###B#C#.#D###
+        #A#D#C#A#
+        #########
+    """.strip.unindent(6).parseInput
+    check leastEnergy(d.burrow, d.highway) == 12081 + 400
 
 # go -----------------------------------------
 
-let data = readFile("./test.txt").parseInput
-echo leastEnergy(data)
+let data = readFile("./input.txt").parseInput
+echo leastEnergy(data.burrow, data.highway)
