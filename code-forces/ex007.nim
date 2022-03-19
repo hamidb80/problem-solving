e6488b73e0f9bdc1fa239d9bf749d52c809c86fc
hamidb80 <hr.bolouri@gmail.com>
2022-03-19 14:37:37 +0330

    add codeforces/ex007

---
+++ code-forces/ex007.nim
@@ -0,0 +1,21 @@
+## problem description: https://codeforces.com/contest/847/problem/B
+
+import std/[strutils, sequtils]
+
+discard stdin.readLine
+var unUsedNumbers = stdin.readLine.splitWhitespace.map parseInt
+
+while unUsedNumbers.len != 0:
+  var usedNumbers: seq[int]
+
+  for un in unUsedNumbers:
+    if usedNumbers.len == 0:
+      usedNumbers.add un
+
+    elif un > usedNumbers[^1]:
+      usedNumbers.add un
+
+
+  echo usedNumbers.join " "
+
+  unUsedNumbers = unUsedNumbers.filterit it notin usedNumbers
