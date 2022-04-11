31a1102ac0035eba9b725892bca663da51f6ab2f
hamidb80 <hr.bolouri@gmail.com>
2022-04-11 10:04:42 +0430

    init main

--- uni/digital-system/eas2json/src/eas2json.nim
+++ uni/digital-system/eas2json/src/eas2json.nim
@@ -1,3 +1,19 @@
-import private/[lisp, serializer]
+import std/[os]
+import private/[lisp, serializer, helper]
 
 export lisp, serializer
+
+# --------------------------
+## project structure:
+##
+## | ease.db
+## | - project.eas
+## | - ...
+## |
+## | toolflow.xml
+## | project.xml
+## | workspace.eas
+
+proc convertProbe*(projectDir, outputDir: string) =
+  withDir projectDir:
+    discard
--- uni/digital-system/eas2json/src/private/helper.nim
+++ uni/digital-system/eas2json/src/private/helper.nim
@@ -7,3 +7,10 @@ const
 
 template err*(msg: string): untyped =
   raise newException(ValueError, msg)
+
+
+template withDir*(dir: string, body: untyped): untyped =
+  let curDir = getCurrentDir()
+  setCurrentDir(dir)
+  body
+  setCurrentDir(curDir)
