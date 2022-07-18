4b52643c896c4736909c0de3ff3bf13bcc8b7719
hamidb80 <hr.bolouri@gmail.com>
2022-07-18 15:59:34 +0430

    add final project

--- uni/data-structure/finalProject/max_heap.py
+++ uni/data-structure/finalProject/max_heap.py
@@ -1,115 +1,128 @@
-from typing import *
-
-
 class MaxHeap:
-    heap: List
-
-    def __init__(self):
-        """
-        Initialize new MaxHeap object.
-        """
-        self.heap = [None]
-
-    @staticmethod
-    def parent(i: int) -> int:
-        """
-            This method is static.
-            Takes an index and returns the index of the parent of this index.
-            :param i: int
-        """
-        return (i - 1) // 2
-
     @staticmethod
     def left_child(i: int) -> int:
-        """
-            This method is static.
-            Takes an index and returns the index of the left child of this index.
-            :param i: int
-        """
-        return ((2 * i) + 1)
+        return (2 * i)
 
     @staticmethod
     def right_child(i: int) -> int:
-        """
-            This method is static.
-            Takes an index and returns the index of the left child of this index.
-            :param i: int
-        """
-        return ((2 * i) + 2)
-
-    def size(self) -> int:
-        """
-        Returns the max-heap size.
-        """
-        pass
-
-    def bubble_up(self, index: int) -> None:
-        """
-        Take an index and bubble up this index.
-        :param index: int
-        """
-        pass
-
-    def bubble_down(self, index: int) -> None:
-        """
-            Take an index and bubble down this index.
-            :param index: int
-        """
-        pass
-
-    def insert(self, item: Any) -> None:
-        """
-        Insert new item in max-heap.
-        :param item:
-        """
-        pass
-
-    def extract_max(self) -> Any:
-        """
-        Return max element of the heap and remove it.
-        """
-        pass
-
-    def find_max(self) -> Any:
-        """
-        Just return max element of the heap.
-        """
-        pass
+        return (2 * i) + 1
 
     @staticmethod
-    def build_heap_with_bubble_up(arr: list[Any]) -> None:
-        """
-        This method is static.
-        Take a list and create a max heap with this elements by bubble-up operation.
-        """
-        pass
+    def parent(i: int) -> int:
+        if i == 1:
+            return 1
+        else:
+            return i // 2
 
-    @staticmethod
-    def build_heap_with_bubble_down(arr: list[Any]) -> None:
-        """
-            This method is static.
-            Take a list and create a max heap with this elements by bubble-up operation.
-        """
-        pass
+    # ---------------------------
 
-    def clear(self) -> None:
-        """
-        Clear max-heap.
-        """
-        pass
+    def __init__(self):
+        self.clear()
+
+    def clear(self):
+        self.data = [None]
+
+    # ---------------------------
+
+    def size(self):
+        return len(self.data)
+
+    def is_empty(self):
+        return self.size() == 1
+
+    def is_branch(self, i: int) -> bool:
+        return self.left_child(i) < self.size()
+
+    def max_child_index(self, i: int):
+        l = self.left_child(i)
+        r = self.right_child(i)
+
+        if r >= self.size() or self.data[l] > self.data[r]:
+            return l
+        else:
+            return r
+
+    def extract_max(self):
+        if self.is_empty():
+            raise "empty"
+        else:
+            return self.pop()
+
+    def find_max(self):
+        return self.data[-1]
+
+    # ---------------------------
+
+    def swap(self, i1, i2):
+        self.data[i1], self.data[i2] = self.data[i2], self.data[i1]
+
+    def bubble_up(self, i):
+        while self.data[i] > self.data[self.parent(i)]:
+            self.swap(i, self.parent(i))
+
+            i = self.parent(i)
+
+    def bubble_down(self, i):
+        while self.is_branch(i):
+            mc = self.max_child_index(i)
+
+            if self.data[i] < self.data[mc]:
+                self.swap(i, mc)
+
+            i = mc
+
+    # ---------------------------
+
+    def push(self, k):
+        self.data.append(k)
+        self.bubble_up(self.size() - 1)
+
+    def pop(self):
+        if len(self.data) == 1:
+            raise "empty"
+
+        max = self.data[1]
+        self.data[1] = self.data[self.size()-1]
+        self.data.pop()
+        self.bubble_down(1)
+        return max
+
+    # ---------------------------
+
+    def add_to_all(self, delta) -> None:
+        for i in range(1, len(self.data)):
+            self.data[i] += delta
+
+    def as_list(self):
+        return self.data[1:]
 
     @staticmethod
     def merge(h1, h2):
-        """
-        This method is static.
-        Takes 2 objects of MaxHeap, merge theirs and returns new MaxHeap object.
-        :param h1: MaxHeap object.
-        :param h2: MaxHeap object.
-        :return: MaxHeap object.
-        """
-        pass
+        new = MaxHeap()
 
-    def add_to_all(self, delta: Any) -> None:
-        """
-        Add delta to each elements in the max-heap.
-        """
+        for n in [*h1.as_list(), *h2.as_list()]:
+            new.push(n)
+
+        return new
+
+
+if __name__ == "__main__":
+    h1 = MaxHeap()
+    h2 = MaxHeap()
+
+    for n in [15, 7, 9, 4, 13, 12, 99]:
+        h1.push(n)
+        h2.push(n)
+
+    h2.add_to_all(2)
+
+    print(h1.as_list())
+    print(h2.as_list())
+
+    # ---------------------
+
+    m = MaxHeap.merge(h1, h2)
+    print(m.as_list())
+
+    while True:
+        print(m.pop())
