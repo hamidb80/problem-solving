155c33b32c84f942e2a9039e9089f6ea3d969aab
hamidb80 <hr.bolouri@gmail.com>
2023-01-17 17:38:24 +0330

    ?

--- advent-of-code/2022/d13/main.nim
+++ advent-of-code/2022/d13/main.nim
@@ -1,22 +1,37 @@
-import std/[sequtils, strutils, strformat, unittest]
-
-{.experimental: "strictFuncs".}
-
-# def ----------------------------------------
+import std/[sequtils, strutils, strformat, unittest, json]
 
 # utils --------------------------------------
 
+func isInt(j: JsonNode): bool =
+  j.kind == Jint
+
+func toInt(j: JsonNode): int =
+  if j.isInt: j.getInt
+  else: j[0].toInt
+
 # implement ----------------------------------
 
-func test(data: string): int =
-  discard
+iterator lists(data: string): JsonNode =
+  for l in data.splitLines:
+    if l != "":
+      yield parseJson l
 
-# tests --------------------------------------
+func correctOrder(a, b: JsonNode): bool =
+  let ints = [a, b].map isInt
 
-test "":
-  check true
+iterator couples[T](s: seq[T]): (int, T, T) =
+  var c = 0
+  for i in countup(0, s.high, 2):
+    inc c
+    yield (c, s[i], s[i+1])
+
+func howManyCorrectOrderOrder(lists: seq[JsonNode]): int =
+  for (i, a, b) in lists.couples:
+    debugEcho (i, correctOrder(a, b))
+    if correctOrder(a, b):
+      inc result, i
 
 # go -----------------------------------------
 
-let data = readFile("./test.txt")
-echo test(data)
+let data = "./test.txt".readFile.lists.toseq
+echo data.howManyCorrectOrderOrder
