19c23696eb7fc91ba32eafba25fdf62c490dfde4
hamidb80 <hr.bolouri@gmail.com>
2023-01-18 13:49:52 +0330

    part2

--- advent-of-code/2022/d13/main.nim
+++ advent-of-code/2022/d13/main.nim
@@ -1,14 +1,14 @@
-import std/[sequtils, strutils, strformat, unittest, json]
+import std/[sequtils, strutils, json, algorithm]
 
-type 
+type
   Compare = enum
-    next
-    right
-    wrong
+    right = -1
+    next = 0
+    wrong = +1
 
   Result = enum
-    equal
     less
+    equal
     more
 
 # utils --------------------------------------
@@ -20,7 +20,7 @@ func toInt(j: JsonNode): int =
   if j.isInt: j.getInt
   else: j[0].toInt
 
-func cmpc[T](a, b: T): Result = 
+func cmpc[T](a, b: T): Result =
   case cast[-1 .. +1](cmp(a, b))
   of -1: less
   of 0: equal
@@ -33,64 +33,71 @@ iterator lists(data: string): JsonNode =
     if l != "":
       yield parseJson l
 
-func correctOrderImpl(a, b: JsonNode, level: int): Compare =
-  let ints = [a, b].map isInt
-
-  # debugEcho ' '.repeat(level*2), (a, b)
-
-  result = 
-    if ints[0] and ints[1]:
-      case cmpc(a.getInt, b.getInt)
-      of equal: next
-      of less: right
-      of more: wrong
-
-    elif ints[0] and not ints[1]:
-      correctOrderImpl(%*[a], b, level+1)
-    
-    elif not ints[0] and ints[1]:
-      correctOrderImpl(a, %*[b], level+1)
-
-    else:
-      var i = 0 
-
-      while i < a.len and i < b.len:
-        let answer = correctOrderImpl(a[i], b[i], level+1)
-
-        case answer
-        of next: discard
-        else: 
-          result = answer
-          break
-
-        inc i
-
-      case result
-      of right, wrong: result
-      of next:
-        case cmpc(a.len ,b.len)
-        of equal: next
-        of less: right
-        of more: wrong
-
-  # debugEcho ' '.repeat(level*2), "result : ", result
-
-func correctOrder(a, b: JsonNode): bool =
-  right == correctOrderImpl(a, b, 1)
-
 iterator couples[T](s: seq[T]): (int, T, T) =
   var c = 0
   for i in countup(0, s.high, 2):
     inc c
     yield (c, s[i], s[i+1])
 
-func correctOrderOrderIndexSum(lists: seq[JsonNode]): int =
-  for (i, a, b) in lists.couples:
-    # debugEcho "\n\n== Pair ", i, " =="
+
+func correctOrderImpl(a, b: JsonNode): Compare =
+  let ints = [a, b].map isInt
+
+  if ints[0] and ints[1]:
+    case cmpc(a.getInt, b.getInt)
+    of equal: next
+    of less: right
+    of more: wrong
+
+  elif ints[0] and not ints[1]:
+    correctOrderImpl(%*[a], b)
+
+  elif not ints[0] and ints[1]:
+    correctOrderImpl(a, %*[b])
+
+  else:
+    var
+      i = 0
+      answer = next
+
+    while i < a.len and i < b.len:
+      answer = correctOrderImpl(a[i], b[i])
+      if answer != next: break
+      inc i
+
+    case answer
+    of right, wrong: answer
+    of next:
+      case cmpc(a.len, b.len)
+      of equal: next
+      of less: right
+      of more: wrong
+
+func correctOrder(a, b: JsonNode): bool =
+  right == correctOrderImpl(a, b)
+
+func cmp(a, b: JsonNode): int =
+  correctOrderImpl(a, b).int
+
+
+func correctOrderOrderIndexSum(packets: seq[JsonNode]): int =
+  for (i, a, b) in packets.couples:
     if correctOrder(a, b):
       inc result, i
 
+func decoderKey(packets: seq[JsonNode]): int =
+  let
+    d2 = %[[2]]
+    d6 = %[[6]]
+
+  result = 1
+
+  for i, packet in sorted(packets & @[d2, d6], cmp):
+    if packet == d2 or packet == d6:
+      result *= i+1
+
 # go -----------------------------------------
 
-let data = "./input.txt".readFile.lists.toseq
-echo data.correctOrderOrderIndexSum
+let data = "./test.txt".readFile.lists.toseq
+echo data.correctOrderOrderIndexSum # 5623
+echo data.decoderKey # 20570
