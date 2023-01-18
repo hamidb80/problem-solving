49715b82f962015c3c0516c0e7abe93a94daeb37
hamidb80 <hr.bolouri@gmail.com>
2023-01-18 12:17:32 +0330

    yay first part

--- advent-of-code/2022/d13/main.nim
+++ advent-of-code/2022/d13/main.nim
@@ -1,5 +1,16 @@
 import std/[sequtils, strutils, strformat, unittest, json]
 
+type 
+  Compare = enum
+    next
+    right
+    wrong
+
+  Result = enum
+    equal
+    less
+    more
+
 # utils --------------------------------------
 
 func isInt(j: JsonNode): bool =
@@ -9,6 +20,12 @@ func toInt(j: JsonNode): int =
   if j.isInt: j.getInt
   else: j[0].toInt
 
+func cmpc[T](a, b: T): Result = 
+  case cast[-1 .. +1](cmp(a, b))
+  of -1: less
+  of 0: equal
+  of +1: more
+
 # implement ----------------------------------
 
 iterator lists(data: string): JsonNode =
@@ -16,8 +33,50 @@ iterator lists(data: string): JsonNode =
     if l != "":
       yield parseJson l
 
-func correctOrder(a, b: JsonNode): bool =
+func correctOrderImpl(a, b: JsonNode, level: int): Compare =
   let ints = [a, b].map isInt
+
+  # debugEcho ' '.repeat(level*2), (a, b)
+
+  result = 
+    if ints[0] and ints[1]:
+      case cmpc(a.getInt, b.getInt)
+      of equal: next
+      of less: right
+      of more: wrong
+
+    elif ints[0] and not ints[1]:
+      correctOrderImpl(%*[a], b, level+1)
+    
+    elif not ints[0] and ints[1]:
+      correctOrderImpl(a, %*[b], level+1)
+
+    else:
+      var i = 0 
+
+      while i < a.len and i < b.len:
+        let answer = correctOrderImpl(a[i], b[i], level+1)
+
+        case answer
+        of next: discard
+        else: 
+          result = answer
+          break
+
+        inc i
+
+      case result
+      of right, wrong: result
+      of next:
+        case cmpc(a.len ,b.len)
+        of equal: next
+        of less: right
+        of more: wrong
+
+  # debugEcho ' '.repeat(level*2), "result : ", result
+
+func correctOrder(a, b: JsonNode): bool =
+  right == correctOrderImpl(a, b, 1)
 
 iterator couples[T](s: seq[T]): (int, T, T) =
   var c = 0
@@ -25,13 +84,13 @@ iterator couples[T](s: seq[T]): (int, T, T) =
     inc c
     yield (c, s[i], s[i+1])
 
-func howManyCorrectOrderOrder(lists: seq[JsonNode]): int =
+func correctOrderOrderIndexSum(lists: seq[JsonNode]): int =
   for (i, a, b) in lists.couples:
-    debugEcho (i, correctOrder(a, b))
+    # debugEcho "\n\n== Pair ", i, " =="
     if correctOrder(a, b):
       inc result, i
 
 # go -----------------------------------------
 
-let data = "./test.txt".readFile.lists.toseq
-echo data.howManyCorrectOrderOrder
+let data = "./input.txt".readFile.lists.toseq
+echo data.correctOrderOrderIndexSum
