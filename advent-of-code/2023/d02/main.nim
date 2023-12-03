d1c3d14a0c07973f179af5812f6b281bdaaac979
hamidb80 <hr.bolouri@gmail.com>
2023-12-03 05:31:12 +0330

    pretty

--- advent-of-code/2023/d02/main.nim
+++ advent-of-code/2023/d02/main.nim
@@ -45,13 +45,12 @@ func parseGame(line: string): Game =
   {.cast(nosideEffect).}:
     discard gameParser.match(line, result)
 
-# implement ----------------------------------
+# utils ----------------------------------
 
-func part1(games: seq[Game]): int =
-  for g in games:
-    if g.sets.allIt(it.red <= 12 and it.green <= 13 and it.blue <= 14):
-      inc result, g.id
-
+func `<=`(a, b: GameSet): bool =
+  a.red <= b.red and
+  a.green <= b.green and
+  a.blue <= b.blue
 
 func max(a, b: GameSet): GameSet =
   GameSet(
@@ -59,12 +58,20 @@ func max(a, b: GameSet): GameSet =
     green: max(a.green, b.green),
     blue: max(a.blue, b.blue))
 
-func innerProduct(gs: GameSet): int =
+func power(gs: GameSet): int =
   gs.red * gs.green * gs.blue
+
+# implement ----------------------------------
+
+func part1(games: seq[Game]): int =
+  let constraint = GameSet(red: 12, green: 13, blue: 14)
+  for g in games:
+    if g.sets.allIt(it <= constraint):
+      inc result, g.id
 
 func part2(games: seq[Game]): int =
   for g in games:
-    inc result, innerProduct g.sets.foldl(max(a, b))
+    inc result, power g.sets.foldl(max(a, b))
 
 # go -----------------------------------------
 
