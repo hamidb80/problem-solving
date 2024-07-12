3960202e13aa5ae147efa2b93a7e40bc4ff2c9b7
hamidb80 <hr.bolouri@gmail.com>
2024-07-12 16:11:34 +0330

    add aoc2015 d4

---
+++ advent-of-code/2015/d4.nim
@@ -0,0 +1,12 @@
+import std/strutils
+import checksums/md5
+
+template str(smth): untyped = $smth
+
+proc smallestMd5startsWith(head, starter: string): int = 
+  for n in 1..100000000:
+    if toMD5(head & $n).str.startsWith starter:
+      return n
+
+echo smallestMd5startsWith("ckczppom", "00000")  # part 1
+echo smallestMd5startsWith("ckczppom", "000000") # part 2
