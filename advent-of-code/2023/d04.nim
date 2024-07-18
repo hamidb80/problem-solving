ee5dfde67b93e425bbb35d91e2489b6d1ad37591
hamidb80 <hr.bolouri@gmail.com>
2024-07-18 12:12:32 +0330

    a whole day

--- advent-of-code/2023/d04.nim
+++ advent-of-code/2023/d04.nim
@@ -41,21 +41,22 @@ func part2(inp: seq[Card]): int =
   for i, (a, b) in inp:
     for n in 1 .. countMatching(a, b):
       inc cards[i+n], cards[i]
+    debugEcho cards, '\n'
   sum cards
 
 
 when isMainModule:
 
   let data = parseInput:
-    readfile"./d04.dat"
-    # """
-    # Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-    # Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-    # Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-    # Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-    # Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-    # Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-    # """
+    # readfile"./d04.dat"
+    """
+    Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    """
 
   stdin.readLine.echo # without this line MS Windows 10 detects the code as virus :-/
   (data.part1, data.part2).echo
