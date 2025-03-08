929f1edc9efc8176d987cd2d6e165428bc6c08a8
hamidb80 <hr.bolouri@gmail.com>
2025-03-09 01:44:38 +0330

    add q12 ch1

--- understanding-cryptography/ch1/q2.nim
+++
@@ -1,6 +0,0 @@
-func shift(t: string, shift: int): string = 
-  for c in t:
-    add result, chr 'a'.int + ((shift + c.int - 'a'.int) mod 26)
-
-for i in 1..25:
-  echo i, " :: ", shift("xultpaajcxitltlxaarpjhtiwtgxktghidhipxciwtvgtpilpitghlxiwiwtxgqadds", i)
