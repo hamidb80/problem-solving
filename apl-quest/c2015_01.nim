d7eab700979e75bc9b4ecf9e28c9cee5516e830d
hamidb80 <hr.bolouri@gmail.com>
2025-03-24 15:04:41 +0330

    pretty

--- apl-quest/c2015_01.nim
+++ apl-quest/c2015_01.nim
@@ -1,17 +1,14 @@
 import std/[strutils, algorithm]
 
-
-template norm(t): untyped = sorted tolower onlyLetters t
-
 func onlyLetters(t: string): string =
   for ch in t:
     if ch in Letters:
       add result, ch
 
-func fn(a, b: string): bool =
-  a.norm == b.norm
+func norm(t: auto)   : auto = sorted tolower onlyLetters t
+func fn(a, b: string): bool = a.norm == b.norm
 
-echo fn("anagram", "Nag A Ram") # 1
-echo fn("Dyalog APL", "Dog Pay All") # 1
-echo fn("", "  !#!") # 1
-echo fn("abcde", "zyxwvu") # 0
+echo "anagram"   .fn "Nag A Ram" # 1
+echo "Dyalog APL".fn "Dog Pay All" # 1
+echo ""          .fn "  !#!" # 1
+echo "abcde"     .fn "zyxwvu" # 0
