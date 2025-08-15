fb8505802c3951fd7a1a9df09261634d22bbcf80
Palestine/humanity is in danger <hr.bolouri@gmail.com>
2025-08-15 05:59:33 +0330

    Update square-roots.jl

--- other/square-roots.jl
+++ other/square-roots.jl
@@ -3,19 +3,11 @@
 
 function solveQuadraic(a, b, c)
   Δ² = b*b - 4*a*c
-
-  if Δ² > 0 
+  if Δ² >= 0 
     x₁ = 0.5 * (-b + sqrt(Δ²)) / a
     x₂ = 0.5 * (-b - sqrt(Δ²)) / a
     [x₁, x₂]
-
-  elseif Δ² == 0 
-    x = -0.5 * b / a
-    [x, x]
-
-  else
-    []
-  end
+  else [] end
 end
 
 # -------------------------------------
